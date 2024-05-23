1575f84f7cae939e96fbc0a41f8315762aa38902
carlos canacue <carlosduardcar@gmail.com>
2024-05-22 20:35:39 -0500

    add ngx loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModules } from './shared/material-mdules';
 import { StartContentComponent } from './start-content/start-content.component';
 import { LoginComponent } from './login/login.component';
+import { NgxUiLoaderConfig, SPINNER } from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig ={
+  text:"Loading...",
+  textColor:"#FFFFF",
+  textPosition:"center-center",
+  bgsColor:"#7b1fa2",
+  fgsColor:"7b1fa2",
+  fgsType:SPINNER.squareJellyBox,
+  fgsSize:100,
+  hasProgressBar:false
+}
 
 @NgModule({
   declarations: [
